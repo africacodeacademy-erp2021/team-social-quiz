b710a7c6de47e8b35937ffe2f6431244d31eb010
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2021-09-23 12:55:51 +0200

    Adding google login authentication

--- src/authentication-and-login/login/login.tsx
+++ src/authentication-and-login/login/login.tsx
@@ -1,25 +1,43 @@
 import React, { useState } from "react";
 import FacebookLogin from 'react-facebook-login';
+import GoogleLogin from 'react-google-login';
 
 
 function Login() {
     const [isLoggedIn, setIsLoggedIn] = useState(false);
-    
+
+
     const responseFacebook = (response: any) => {
-        console.log(response);
-        setIsLoggedIn(true);
+      console.log(response);
+      setIsLoggedIn(true);
     }
+  
+  const responseGoogle = (response: any )=>{
+    console.log(response);
+    console.log(response.profileObj);
+    setIsLoggedIn(true);
+  }
+  
     return (
-        <div className="App">
-            {!isLoggedIn ?
-                <FacebookLogin
-                    appId="390822489345318"
-                    autoLoad={true}
-                    fields="name,email,picture"
-                    callback={responseFacebook} /> : 'Home'}
-        </div>
+      <div className="App">
+        {!isLoggedIn ?
+          <FacebookLogin
+            appId="390822489345318"
+            autoLoad={true}
+            fields="name,email,picture"
+            //onClick={componentClicked}
+            callback={responseFacebook} /> : 'Home'}
+        <br/>
+        {!isLoggedIn ? <GoogleLogin
+          clientId="791329261171-nvioqksgkqu74v86vnoh6qv7ocha9qbf.apps.googleusercontent.com"
+          buttonText="LOGIN WITH GOOGLE"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={'single_host_origin'}
+        />: 'Home'}
+      </div>
     );
-}
+  }
 
 export default Login;
 
