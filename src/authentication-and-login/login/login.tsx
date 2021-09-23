cb9603b0f8de1ee5059d33ba5046ac844730eb1b
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2021-09-23 12:24:20 +0200

    Adding Facebook-login authorization

--- src/authentication-and-login/login/login.tsx
+++ src/authentication-and-login/login/login.tsx
@@ -1,4 +1,25 @@
-export default function login(){
+import React, { useState } from "react";
+import FacebookLogin from 'react-facebook-login';
 
-   
+
+function Login() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    
+    const responseFacebook = (response: any) => {
+        console.log(response);
+        setIsLoggedIn(true);
+    }
+    return (
+        <div className="App">
+            {!isLoggedIn ?
+                <FacebookLogin
+                    appId="390822489345318"
+                    autoLoad={true}
+                    fields="name,email,picture"
+                    callback={responseFacebook} /> : 'Home'}
+        </div>
+    );
 }
+
+export default Login;
+
