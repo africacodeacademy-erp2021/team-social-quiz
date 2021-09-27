26cdbcdda56bdfb574c72a0c98b1a7986b7765f7
Molati <86184198+Molati@users.noreply.github.com>
2021-09-26 18:57:31 -0700

    commit questions and answers Next button

--- src/game/Questions/general-questions.tsx
+++ src/game/Questions/general-questions.tsx
@@ -120,180 +120,151 @@ export default function General_questions(){
   
     ];
   
-  
-  const [currentQuestion, SetCurrentQuestion] = useState(0);
-  const [showScore, SetShowScore] = useState(false);
-  const [count, setCount] = useState(0);
-  const allowedUser = "Host";
-  const userRole = "Guest";
-  
-  const [selected_number, setSelected_number] = useState(10);
-  const [score, SetScore] = useState(0);
-  const space = " . "; 
-
-
-/**
- * Set number of questions on load
- * @param {number} selected_number
-*/    
-  window.addEventListener("load", function (){
-   
-     setSelected_number(5);
-     
-     return selected_number;
-  });
-  
- 
-  
-/**
- * add 1 to score if answer is correct 
- *Set count to 1 to Make sure the user gives an answer once
- * @param {boolean} isCorrect 
-*/
-  const handleAnswerButtonClick = (isCorrect:boolean) => {
-        
-    if (isCorrect === true) {
-      
-          if(count === 0){
-             setCount(1);
-               SetScore(score + 1);
-                
-                 }else{
-
-                   alert("Your answer was Captured");
-              }
-
-    }else
-          if(isCorrect === false){
-
-               if(count === 0){
-                   setCount(1);
-               
-                      }else{
-   
-                      alert("Your answer was Captured");
-                 }          
-          }            
-  }
-
-
-
-/**
-* Move to next question if Player Role Host
-* @param {string} userRole 
-*/
-  const onNextquestion = ()=>{ 
-  const nextQuetions = currentQuestion + 1;
+    const [currentQuestion, SetCurrentQuestion] = useState(0);
+    const [showScore, SetShowScore] = useState(false);
+    const [showNext, SetShowNext] = useState(false);
+    const [scoreStatus, SetScoreStatus] = useState(false);
+    let userRole = 'Host';  
+    const [selected_number, setSelected_number] = useState(10);
+    let [score, SetScore] = useState(0);
+    const space = " . "; 
     
-   if(JSON.stringify(userRole) === JSON.stringify(allowedUser)){
-
-        if (nextQuetions < selected_number) {
-
-            //set count to 0 on next question
-            setCount(0);
-            SetCurrentQuestion(nextQuetions);
-
-            }
-            else {
-                    SetShowScore(true)
-            }
-     }else{
-
-        alert("Sorry...Only The Host Can Move To The Next Question");
-   }
- }
-
-
-
-/**
-* Show message depending on how well User Played
-* @param {number} score 
-*/
-  const totalScore = selected_number * 1;
-
-  const halfOfScore = totalScore / 2;
-  let message = ""; 
-  
-  if(score >= halfOfScore){
-   
-         message ="Congratulation You Won the Game!!";
-            
-  }
-    else
-       {
-         message = "Sorry....Try Again";             
-      
-       }
-
+    /**
+     * Set number of questions on load
+     * Display Next button if player is Host
+     * @param {number} selected_number
+    */    
+     window.addEventListener("load", function (){  
+          if(userRole === 'Host'){
+             SetShowNext(true);
+          } 
+          setSelected_number(5);     
+     });
        
-
-  return (
+    /**
+    * Set score status
+    *  @param {boolean} isCorrect 
+    */
+    const handleAnswerButtonClick = (isCorrect:boolean) => {     
+        if (isCorrect === true) {           
+                 SetScoreStatus(true);    
+            } else{
+                SetScoreStatus(false);
+        }   
+    }
   
-  <div className="question_display">
+    /**
+    * add 1 to score if answer is correct 
+    *  @param {number} score
+    */
+    function addScoreOnNext(){       
+        if(scoreStatus === true){
+                SetScore(score +=1);
+                SetScoreStatus(false);   
+        }
+    }
     
-     <div className="body">
-         <div><h3>Progress Bar</h3></div>
-         <div><h4>Audio && other's section</h4></div>
-         
-
-        <>
-  
-          <div className="answers">
+    /**
+    * Move to next question
+    * @param {number} nextQuetions 
+    */
+     const onNextquestion = ()=>{ 
+          let nextQuetions = currentQuestion + 1;
+          addScoreOnNext();
+    
+          if (nextQuetions < selected_number){                             
+                       SetCurrentQuestion(nextQuetions);
+            }else {
+                       SetShowScore(true)
+                  }
+      }
+    
+    /**
+    * Show message depending on how well User Played
+    * @param {number} score 
+    */
+      const totalScore = selected_number * 1;
+      const halfOfScore = totalScore / 2;
+      let message = ""; 
+    
+      if(score >= halfOfScore){   
+             message ="Congratulation You Won the Game!!";
+      }else{
+             message = "Sorry....Try Again";             
+      }
+    
+      return (
+      
+      <div className="question_display">
         
-            {showScore ? (
-  
-              <div className='final_score_section'>      
-                 You scored {score} out of {selected_number}
-  
-                      <br/>
-                          {message}
-                      <br/>                      
-
-              </div>
-            )
-            :
-            (
-            
-            <>
-                
-                <div>
-                  <div className='questions-section' >
-                        <span>{currentQuestion + 1 }
-                       
-                          {space}
-                       
-                       </span>{questions[currentQuestion].questionText}
-                   
-                   </div>
-                </div>
-   
-             <div className='answer-section'>
-                   { 
-                      questions[currentQuestion].answerOptions.map((answerOptions) => (
-  
-                          <button className='answer-button' onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)}>
-                          {answerOptions.answerText}</button>
-                         
-                      ))                     
-                   }
-             </div>
-
-             <div >
-             <br/>
-             <br/>    
-             <button className="onNext" onClick={onNextquestion} >Next</button>
-             </div>
-
-        </>
-      )}
-    
-  </div>
-
-  
-  </>
+         <div className="body">
+             <div><h3>Progress Bar</h3></div>
+             <div><h4>Audio && other's section</h4></div>
              
-  </div>
-   
-  </div>
+    
+            <>
+      
+              <div className="answers">
+            
+                {showScore ? (
+      
+                  <div className='final_score_section'>      
+                     You scored {score} out of {selected_number}
+      
+                          <br/>
+                              {message}
+                          <br/>                      
+    
+                  </div>
+                )
+                :
+                (
+                
+                <>
+                    
+                    <div>
+                      <div className='questions-section' >
+                            <span>{currentQuestion + 1 }
+                           
+                              {space}
+                           
+                           </span>{questions[currentQuestion].questionText}
+                       
+                       </div>
+                    </div>
+       
+                 <div className='answer-section'>
+                       { 
+                          questions[currentQuestion].answerOptions.map((answerOptions) => (
+      
+                              <button className='answer-button' onClick={() => handleAnswerButtonClick(answerOptions.isCorrect)}>
+                              {answerOptions.answerText}</button>
+                             
+                          ))                     
+                       }
+                 </div>
+    
+                 <div >
+                   <br/>
+                   <br/>    
+                       {   
+                         showNext ? (                      
+                                       <button className="onNext" onClick={onNextquestion} > Next </button>
+                                     ):( <></>          
+                             )
+                       }
+                 </div>
+    
+            </>
+          )}
+        
+      </div>
+    
+      
+      </>
+                 
+      </div>
+       
+      </div>
 );
 }
