227e5be019ac178ef62b9caf5d087fb7ebc07506
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2021-10-06 13:15:11 +0200

    adding a google authentication component

---
+++ src/authentication-and-login/google_authentication/google.tsx
@@ -0,0 +1,54 @@
+import GoogleLogin from "react-google-login";
+import * as dotenv from "dotenv";
+import { BrowserRouter as Router, Route, Switch, useHistory } from "react-router-dom";
+import Landing from "../../dashboard/Landing";
+
+
+export default function Google(){
+   
+  function Content(){
+    dotenv.config();
+    let history = useHistory();
+
+    
+
+const loginsuccess = (response: any) => {
+    localStorage.setItem("name", response.dt.uU);
+     history.push("./Landing");
+  };
+
+  const clientId = JSON.stringify(process.env.REACT_APP_GOOGLE_CLIENT_ID);
+  const name = clientId.replace('"', "");
+  const Clientid = name.replace('"', "");
+
+  return(
+      <div>
+          <button className="btn_google" >
+             <GoogleLogin
+             
+                  className="btn_googleinfo"
+                  clientId={Clientid}
+                  buttonText="SignIn with Google"
+                  onSuccess={loginsuccess}
+                  onFailure={loginsuccess}
+                  cookiePolicy={"single_host_origin"}
+                />
+                </button>
+              </div>
+      
+  )
+  }
+  return (
+    <div>
+      <Router>
+        <Switch>
+          <Route path="/Landing" exact component={Landing} />
+          
+
+          <Route path="/" component={Content} />
+        </Switch>
+      </Router>
+    </div>
+  );
+
+}      
