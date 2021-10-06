b6f61e8809b0f9c8c20f2b5fb0962857024b2008
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2021-10-06 13:18:26 +0200

    Calling authentication components in login.tsx

--- src/authentication-and-login/login/login.tsx
+++ src/authentication-and-login/login/login.tsx
@@ -1,43 +1,81 @@
-import React, { useState } from "react";
-import FacebookLogin from 'react-facebook-login';
-import GoogleLogin from 'react-google-login';
+import "../../App.css";
+import Display_img from "../../game/images/Rubix Cube Image.jpg";
 
+import * as dotenv from "dotenv";
+import { BrowserRouter as Router, Route, Switch, Link,  } from "react-router-dom";
+import Registration from "../registration/registration";
+
+import Google from "../google_authentication/google";
+import Facebook from "../facebook_authentication/facebook";
+import Github from "../github_authentication/github";
 
 function Login() {
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+  function Content() {
+    dotenv.config();
 
 
-    const responseFacebook = (response: any) => {
-      console.log(response);
-      setIsLoggedIn(true);
-    }
-  
-  const responseGoogle = (response: any )=>{
-    console.log(response);
-    console.log(response.profileObj);
-    setIsLoggedIn(true);
-  }
-  
     return (
-      <div className="App">
-        {!isLoggedIn ?
-          <FacebookLogin
-            appId="390822489345318"
-            autoLoad={true}
-            fields="name,email,picture"
-            //onClick={componentClicked}
-            callback={responseFacebook} /> : 'Home'}
-        <br/>
-        {!isLoggedIn ? <GoogleLogin
-          clientId="791329261171-nvioqksgkqu74v86vnoh6qv7ocha9qbf.apps.googleusercontent.com"
-          buttonText="LOGIN WITH GOOGLE"
-          onSuccess={responseGoogle}
-          onFailure={responseGoogle}
-          cookiePolicy={'single_host_origin'}
-        />: 'Home'}
+      <div className="all_sections">
+        <div className="display_infor">
+          <div className="login">
+            <header>
+              {" "}
+              <h1 className="h1_title">
+                {" "}
+                <span className="designcolor">quiz</span>app
+              </h1>{" "}
+            </header>
+
+            <div>
+              <h2 className="h2_title">Sign In</h2>
+              <h3 className="h3_title">See your growth and get more quizes</h3>
+            </div>
+            <br />
+            <br />
+            <br />
+            <div className="buttons_container">
+              <Google/>
+              <br />
+              <Facebook />
+              <br />
+              <Github />
+            </div>
+          </div>
+
+          <br />
+          <div className="nav_newAccount">
+            <p className="p_title">Not registered yet ? </p>{" "}
+            <p className="p_title">
+              <Link className="link_text" to="/registration">
+                Create an Account
+              </Link>
+            </p>
+          </div>
+        </div>
+
+        <img
+          className="my_display"
+          src={Display_img}
+          width="820px"
+          height="800px"
+          alt="logo"
+        />
       </div>
     );
   }
 
-export default Login;
 
+return (
+  <div>
+    <Router>
+      <Switch>
+        <Route path="/registration" exact component={Registration} />
+        
+
+        <Route path="/" component={Content} />
+      </Switch>
+    </Router>
+  </div>
+);
+}
+export default Login;
