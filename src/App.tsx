153a3734630720cbe1f91c5835df62fbb8744c4c
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2021-10-07 13:03:05 +0200

    Adding design images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { Route, Switch } from "react-router-dom";
+import "./App.css";
+import Login from "./authentication-and-login/login/login";
 
+import Registration from "./authentication-and-login/registration/registration";
+import Category from "./dashboard/Category";
+import Landing from "./dashboard/Category";
 
 function App() {
   return (
     <div className="App">
-     
+      <Switch>
+        <Route path="/" exact>
+          <Login />
+        </Route>
+        <Route path="/Category">
+          <Category />
+        </Route>
+        <Route path="/registration">
+          <Registration />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
