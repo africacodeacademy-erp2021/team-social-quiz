3bf94bd6db789912a96b558f0b092251e6cd14b7
Molati <86184198+Molati@users.noreply.github.com>
2021-10-15 12:35:30 -0700

    commit remove play button

--- src/game/Game_Components/audio.tsx
+++ src/game/Game_Components/audio.tsx
@@ -2,7 +2,6 @@ import './audio.css';
 import {useEffect, useState } from 'react';
 
 function My_audio(){
-const [buttonPlayStatus, SetButtonPlayStaton] = useState(true);
 const [buttonMuteStatus, SetbuttonMuteStatus] = useState(true);
 let [count, SetCount] = useState(1);
 
@@ -15,24 +14,13 @@ var playbtn:any, mutebtn:any;
          audio.play();
     
         //reference objects
-        playbtn = document.getElementById("play_And_pause");
         mutebtn = document.getElementById("muteAndUnmute");
     
         //Handle event
-        playbtn.addEventListener("click",playPause);
-        mutebtn.addEventListener("click", mute);
+         mutebtn.addEventListener("click", mute);
     
         //audio functions
-    function playPause(){
-          if(audio.paused){
-              audio.play();
-            SetButtonPlayStaton(false);
-            } else {
-              audio.pause();
-            SetButtonPlayStaton(true);
-            }
-        }
-    function mute(){
+       function mute(){
           if(audio.muted){
               audio.muted = false;
               SetbuttonMuteStatus(true);
@@ -52,16 +40,7 @@ useEffect(()=>{
     
    return(
         <div className="btn_section"> 
-        {buttonPlayStatus ?(
-            <button className="btnplay" id="play_And_pause"> 
-              <img className="audioIcon" src="https://cdn4.iconfinder.com/data/icons/media-player-icons/80/Media_player_icons-12-256.png" alt="icon"/>
-            </button>
-          ):(
-            <button className="btnplay" id="play_And_pause">
-              <img className="audioIcon" src="https://cdn1.iconfinder.com/data/icons/glyphie-1/40/stop_play_music_video_audio_parallel_button-512.png" alt="icon"/>
-            </button>
-          )
-        }
+ 
         {buttonMuteStatus ?(
             <button className="btnmute" id="muteAndUnmute">
               <img className="audioIcon" src="https://cdn2.iconfinder.com/data/icons/ui-minimalist-0-1-1/16/UI_Web_Sound_Tone_Music_Volume-512.png" alt="icon"/>
--- src/game/Game_Play/game_Memes.tsx
+++ src/game/Game_Play/game_Memes.tsx
@@ -1,14 +1,20 @@
 import './playgame.css';
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 import EndButton from '../Game_Components/endButton';
 import { GameContext } from './game_Context';
 import { useHistory } from 'react-router';
+import {CongratsMemes} from '.././memeURL/congratsMemes';
+import {TryAgainMemes} from '.././memeURL/tryAgainMeme';
 
 function My_Memes(){
 const {SetGameState} = useContext(GameContext); 
 const {scoreStatus, SetScoreStatus} = useContext(GameContext);
 const {lastQuestion} =  useContext(GameContext);
 const {playerName} =  useContext(GameContext);
+
+let   [countCongrats, SetCountCongrats] = useState(0);
+let   [countTry, SetTry] = useState(0);
+
 let {score} = useContext(GameContext);
 localStorage.setItem("PlayerScore",score);
 let showImg = "";
@@ -16,28 +22,46 @@ let message = "";
 let pointsMessage = "";
 let history = useHistory();
 
+//change Memes index value
+function nextMemes(){  
+  if(scoreStatus === true){    
+    SetCountCongrats(countCongrats++); 
+        if(countCongrats === 4){
+            SetCountCongrats(0);
+        }
+        console.log("Congrats = "+countCongrats);
+    }else{
+        SetTry(countTry++); 
+        if(countTry === 4){
+            SetCountCongrats(0);
+        }
+        console.log("Try = "+countTry);
+    }
+} 
+
 /**
+* call function nextMemes
 * Set gamestate back to Playgame
 * set ScoreStatus to false before moving to the next question
 * set gamestate to lastmeme if this is the last question
 * @param {boolean} GameState
 */  
 function NextQuestion(){
-    SetGameState("PlayGame"); 
-    SetScoreStatus(false); 
-    if(lastQuestion === true){
-       history.push("/score");
-    }
+       nextMemes();
+        SetGameState("PlayGame"); 
+        SetScoreStatus(false); 
+        if(lastQuestion === true){
+        history.push("/score");
+        }
 }
 
 // Meme is displayed depanding on the answer
 if(scoreStatus === true){
-    showImg = 'https://memegenerator.net/img/instances/64115112/well-done.jpg';
+    showImg =  CongratsMemes[countCongrats].memeURL
     message = 'Quizapp stans!!';
     pointsMessage ="+50 points";
-     
 }else{
-    showImg = 'https://www.memecreator.org/static/images/memes/3218379.jpg';
+    showImg =  TryAgainMemes[countTry].memeURL
     message = 'Dont worry you can still get it right.';
     pointsMessage ="0 points";
 }
