1f4b1c011149e6e157a193fcf2ba17d893131281
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2021-10-20 13:02:02 +0200

    Adding more tests

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+
 
 
 ReactDOM.render(
   <BrowserRouter>
     <App />
- </BrowserRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
