dad6fa4facbf39022ca23b904781fa985431ec79
Majoro Futhoane <85506666+AzazelEmjay@users.noreply.github.com>
2021-11-18 15:15:45 +0200

    Removing auth rendering in our tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,15 +1,13 @@
-import { mount, shallow } from "enzyme";
+import {  shallow } from "enzyme";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 import { configure } from "enzyme";
 
 import "@testing-library/jest-dom";
 import React from "react";
-import Facebook from "./authentication-and-login/facebook_authentication/facebook";
-import Github from "./authentication-and-login/github_authentication/github";
-import Google from "./authentication-and-login/google_authentication/google";
+
 import Login from "./authentication-and-login/login/login";
 
-import {  render, screen } from "@testing-library/react";
+
 
 
 
@@ -24,29 +22,7 @@ describe("Login Page", () => {
   });
 });
 
-  it('check if Authentication popup opens after clicking on Google button', () => {  
-    const login = mount(<Google />),
-        buttonclick = login.find("button");
-       
-    buttonclick.first().simulate('click');
-    expect(login.find('button')).toHaveLength(1);
-});
-
-
-test('Authentication button exists', async () => {
-    render(<Facebook />);
  
-    const button = screen.getByRole('button', { name: 'Login with Facebook' });
-    expect(button).toBeInTheDocument();
-})
-
-test('Props values are passed correctly', async () => {
-    render(<Facebook buttonName="SignIn with Facebook"/>);
- 
-    const signIn = screen.getByRole('button', { name: 'SignIn with Facebook' });
-    expect(signIn).toBeInTheDocument();
-});
-
 
 
 
